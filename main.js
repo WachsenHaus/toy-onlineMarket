fb667699bbbb240244a16a1e56efda89fdd10b51
WachsenHaus <59411545+WachsenHaus@users.noreply.github.com>
2020-05-12 22:37:00 +0900

    정렬기능,분류기능 추가했습니다.
    타입,컬러별로 선택시 정렬,분류 되는기능 추가했습니다.

--- main.js
+++ main.js
@@ -1,223 +1,256 @@
+"use strict";
+
 const storeData = {
-    clothes : [
-        {
-            color : "pink",
-            size : "small",
-            part : 'shirt',
-            sex : "man"
-        },
-        {
-            color : "yellow",
-            size : "small",
-            part : 'pants',
-           sex : "man"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'skirt'
-            ,sex : "man"
-        },
-        {
-            color : "blue",
-            size : "large",
-            part : 'skirt'
-            ,sex : "man"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'pants',sex : "man"
-        },
-        {
-            color : "blue",
-            size : "large",
-            part : 'pants',sex : "woman"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "blue",
-            size : "large",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "blue",
-            size : "large",
-            part : 'pants',sex : "man"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'pants',sex : "man"
-        },
-        {
-            color : "blue",
-            size : "large",
-            part : 'skirt',sex : "man"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "yellow",
-            size : "large",
-            part : 'pants',sex : "woman"
-        },
-        {
-            color : "yellow",
-            size : "large",
-            part : 'skirt',sex : "man"
-        },
-        {
-            color : "yellow",
-            size : "large",
-            part : 'pants',sex : "woman"
-        },
-        {
-            color : "yellow",
-            size : "small",
-            part : 'pants',sex : "man"
-        },
-        {
-            color : "pink",
-            size : "large",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "blue",
-            size : "small",
-            part : 'skirt',sex : "woman"
-        },
-        {
-            color : "pink",
-            size : "small",
-            part : 'skirt',sex : "man"
-        },
-        {
-            color : "pink",
-            size : "small",
-            part : 'skirt',sex : "woman"
-        },
-    ]
-}
+  clothes: [
+    {
+      color: "pink",
+      size: "small",
+      part: "t",
+      sex: "man",
+    },
+    {
+      color: "yellow",
+      size: "large",
+      part: "t",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "small",
+      part: "t",
+      sex: "woman",
+    },
+    {
+      color: "pink",
+      size: "small",
+      part: "t",
+      sex: "woman",
+    },
+    {
+      color: "pink",
+      size: "small",
+      part: "t",
+      sex: "man",
+    },
+    {
+      color: "yellow",
+      size: "small",
+      part: "p",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "s",
+      sex: "man",
+    },
+    {
+      color: "blue",
+      size: "large",
+      part: "s",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "p",
+      sex: "man",
+    },
+    {
+      color: "blue",
+      size: "large",
+      part: "p",
+      sex: "woman",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "blue",
+      size: "large",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "blue",
+      size: "large",
+      part: "p",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "p",
+      sex: "man",
+    },
+    {
+      color: "blue",
+      size: "large",
+      part: "s",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "yellow",
+      size: "large",
+      part: "p",
+      sex: "woman",
+    },
+    {
+      color: "yellow",
+      size: "large",
+      part: "s",
+      sex: "man",
+    },
+    {
+      color: "yellow",
+      size: "large",
+      part: "p",
+      sex: "woman",
+    },
+    {
+      color: "yellow",
+      size: "small",
+      part: "p",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "large",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "blue",
+      size: "small",
+      part: "s",
+      sex: "woman",
+    },
+    {
+      color: "pink",
+      size: "small",
+      part: "s",
+      sex: "man",
+    },
+    {
+      color: "pink",
+      size: "small",
+      part: "s",
+      sex: "woman",
+    },
+  ],
+};
 
 let colorPicker = null;
 let typePicker = null;
 
 const drawItemContainer = document.getElementsByClassName("item_container");
-const storeList = document.getElementById('item_container-storeList');
+const storeList = document.getElementById("item_container-storeList");
 
+function init() {
+  const tBtn = document.getElementById("typeTop");
+  const pBtn = document.getElementById("typePants");
+  const sBtn = document.getElementById("typeSkirt");
+  const blueBtn = document.getElementById("colorBlue");
+  const yellowBtn = document.getElementById("colorYellow");
+  const pinkBtn = document.getElementById("colorPink");
 
-function init()
-{
-    const shirtBtn = document.getElementById('typeShirt');
-    const pantsBtn = document.getElementById('typePants');
-    const skirtBtn = document.getElementById('typeSkirt');
-    const blueBtn = document.getElementById('colorBlue');
-    const yellowBtn = document.getElementById('colorYellow');
-    const pinkBtn = document.getElementById('colorPink');
+  tBtn.addEventListener("click", picker);
+  pBtn.addEventListener("click", picker);
+  sBtn.addEventListener("click", picker);
+  blueBtn.addEventListener("click", picker);
+  yellowBtn.addEventListener("click", picker);
+  pinkBtn.addEventListener("click", picker);
 
-    shirtBtn.addEventListener('click',picker);
-    shirtBtn.addEventListener('click',picker);
-    pantsBtn.addEventListener('click',picker);
-    skirtBtn.addEventListener('click',picker);
-    blueBtn.addEventListener('click',picker);
-    yellowBtn.addEventListener("click",picker);
-    pinkBtn.addEventListener("click",picker);
-
-
-    // console.log(storeData.clothes[0].color);
+  drawStore();
+  // console.log(storeData.clothes[0].color);
 }
-function picker(event){
-    switch(event.target.id)
-    {
-        case 'typeShirt':
-            typePicker = "t";
-            break;
-        case 'typePants':
-            typePicker = "p";
-            break;
-        case 'typeSkirt':
-            typePicker = "s";
-            break;
-        case 'colorBlue':
-            colorPicker = "blue";
-            break;
-        case 'colorYellow':
-            colorPicker = "yellow";
-            break;
-        case 'colorPink':
-            colorPicker = "pink";
-            break;
-    }
-    drawStore();
+function picker(event) {
+  switch (event.target.id) {
+    case "typeTop":
+      typePicker = "t";
+      break;
+    case "typePants":
+      typePicker = "p";
+      break;
+    case "typeSkirt":
+      typePicker = "s";
+      break;
+    case "colorBlue":
+      colorPicker = "blue";
+      break;
+    case "colorYellow":
+      colorPicker = "yellow";
+      break;
+    case "colorPink":
+      colorPicker = "pink";
+      break;
+  }
+  drawStore();
 }
 
-function drawStore(){
-    //그리기전에 기존에 있던 html 요소들 삭제.
-    const ul = document.getElementById("item_container-storeList");
-    while ( ul.hasChildNodes() ) 
-    { 
-        ul.removeChild( ul.firstChild );
-    }
+function drawStore() {
+  //그리기전에 기존에 있던 html 요소들 삭제.
+  const ul = document.getElementById("item_container-storeList");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
 
-    for (var prop in storeData.clothes) {
-        const part = storeData.clothes[prop].part;
-        const size =storeData.clothes[prop].size;
-        const color = storeData.clothes[prop].color;
-        const sex = storeData.clothes[prop].sex;
-        paintStoreItem(part,size,color,sex);
-    }
+  for (let prop in storeData.clothes) {
+    const part = storeData.clothes[prop].part;
+    const size = storeData.clothes[prop].size;
+    const color = storeData.clothes[prop].color;
+    const sex = storeData.clothes[prop].sex;
+    paintStoreItem(part, size, color, sex);
+  }
 }
 
-function paintStoreItem(part,size,color,sex)
-{
-    const li = document.createElement("li");
-    // const delBtn = document.createElement("buttn");
-    const img = document.createElement("img");
-    const spanSex = document.createElement("span");
-    const space = document.createElement("span");
-    const spanSize = document.createElement("span");
-    // const newId = toDos.length + 1;
+function paintStoreItem(part, size, color, sex) {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  const spanSex = document.createElement("span");
+  const Separator = document.createElement("span");
+  const spanSize = document.createElement("span");
 
-    // delBtn.innerText = "❌";
-    // // delBtn.addEventListener("click",deleteToDo);
-    if(typePicker === null)
-    {
-        
-    }
-    img.src = `./imgs/${colorPicker}_${typePicker}.png`;
-    spanSex.innerText = sex;
-    space.innerText = ',';
-    spanSize.innerHTML = size + ' size';
-    // li.appendChild(delBtn);
-    li.appendChild(img);
-    li.appendChild(spanSex);
-    li.appendChild(space);
-    li.appendChild(spanSize);
-    // li.id = newId;
+  img.src = `./imgs/${color}_${part}.png`;
+  spanSex.innerText = sex;
+  Separator.innerText = ",";
+  spanSize.innerHTML = size + " size";
+  li.appendChild(img);
+  li.appendChild(spanSex);
+  li.appendChild(Separator);
+  li.appendChild(spanSize);
+  //모두 null인 초기 상태일때는 조건없이 모든 상점 데이터를 출력한다.
+  if (typePicker === null && colorPicker === null) {
     storeList.appendChild(li);
-    // const toDoObj =
-    // {
-    //     text : text,
-    //     id : newId,
-    // }
-    // toDos.push(toDoObj);
-    // saveToDos();
-    
+  }
+  // 옷의 종류나 색상이 선택될경우 해당 조건의 상점 데이터만 출력한다.
+  if (typePicker === null || colorPicker === null) {
+    if (typePicker === part) {
+      storeList.appendChild(li);
+    }
+    if (colorPicker === color) {
+      storeList.appendChild(li);
+    }
+  }
+  //선택된 조건의 상점 데이터만 출력한다.
+  else if (typePicker === part && colorPicker === color) {
+    storeList.appendChild(li);
+  }
 }
-
 
 init();
-
